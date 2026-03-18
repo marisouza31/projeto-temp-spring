92bcc3991e34b22029d57a028c2fded3b490ed90
marisouza31 <marianaocireu@gmail.com>
2026-03-18 20:02:59 -0300

    projeto spring"

---
+++ demo/demo/src/test/java/com/fatec/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
